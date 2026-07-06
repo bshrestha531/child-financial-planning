46849dc385d0361ca35fe2dd4a1267b84c4d332d
Bijesh Shrestha <bijesh.shrestha@westpoint.edu>
2026-07-06 16:17:23 -0400

    nil

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import HeadStartPlanner from "./HeadStartPlanner.jsx";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <HeadStartPlanner />
-  </React.StrictMode>
-);
